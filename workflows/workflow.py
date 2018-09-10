6d0c90309e90414ae1d9f4b36fb8ddf0d4e6924e
Unknown <cld100@pitt.edu>
2018-09-10 14:43:01 -0400

    update

--- workflows/workflow.py
+++ workflows/workflow.py
@@ -94,11 +94,14 @@ def variant_call_workflow(reference: Path, sample: common.Sample, **kwargs):
 def dmux_workflow(reference:Path, dmux_folder:Path, output_folder:Path):
 
 	dmux_samples = list(i for i in dmux_folder.glob("**/*") if (i.suffix == '.gz'))
+
 	dmux_samples = filter(lambda s: s.stem.split('_')[3] not in {'I1', 'I2'}, dmux_samples)
-	groups = groupby(dmux_samples, lambda s: '_'.join(s.stem.split('_')[:2]))
+
+	groups = groupby(dmux_samples, lambda s: '_'.join(s.stem.split('_')[:3]))
+
 	print(len(groups))
 	index = 0
-	for stem, samples in groups.items():
+	for stem, samples in sorted(groups.items()):
 		index += 1
 		print("Processing {} ({} of {})".format(stem, index, len(groups)))
 		forward_read = [i for i in samples if i.stem.split('_')[3] == 'R1'][0]
