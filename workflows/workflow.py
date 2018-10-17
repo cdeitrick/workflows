1d3c1ae66b568add9d6e5e3e84db69a7129c378e
Unknown <cld100@pitt.edu>
2018-10-17 11:38:40 -0400

    fix

--- workflows/workflow.py
+++ workflows/workflow.py
@@ -1,6 +1,6 @@
 import sys
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Tuple
 
 sys.path.append(str(Path(__file__).parent))
 
@@ -43,7 +43,7 @@ def groupby(collection, by) -> Dict[Any, List[Any]]:
 	return groups
 
 
-def assemble_workflow(forward_read: Path, reverse_read: Path, parent_folder: Path, genus:str, species:str):
+def assemble_workflow(forward_read: Path, reverse_read: Path, parent_folder: Path, genus: str, species: str):
 	forward_name = forward_read.stem
 	reverse_name = reverse_read.stem
 	sample_name = first_common_substring(forward_name, reverse_name)
@@ -64,14 +64,11 @@ def assemble_workflow(forward_read: Path, reverse_read: Path, parent_folder: Pat
 	)
 	return spades_output
 
-def variant_call_workflow(sample_name:Path, forward_read: Path, reverse_read:Path, parent_folder:Path, reference:Path):
 
-	#annotation.prokka(spades_output.contigs, prokka_output_folder, prokka_options)
-
+def variant_call_workflow(sample_name: Path, forward_read: Path, reverse_read: Path, parent_folder: Path, reference: Path):
+	# annotation.prokka(spades_output.contigs, prokka_output_folder, prokka_options)
 
 	sample_folder = common.checkdir(parent_folder / sample_name)
-
-
 
 	trimmomatic_options = read_quality.TrimmomaticOptions()
 	print(sample_name)
@@ -97,37 +94,55 @@ def variant_call_workflow(sample_name:Path, forward_read: Path, reverse_read:Pat
 	return breseq_output
 
 
+def get_sample(sample_id: str, folder: Path) -> Tuple[Path, Path]:
+	candidates = [i for i in folder.glob("**/*") if i.suffix == '.gz']
+	candidates = [i for i in candidates if i.name.startswith(sample_id)]
+
+	forward = [i for i in candidates if 'R1' in i]
+
+	reverse = [i for i in candidates if 'R2' in i]
+	print(forward)
+	print(reverse)
+	return forward[0], reverse[0]
+
+
 if __name__ == "__main__":
+	import pendulum
 	import pandas
-	#import logging
-	_sequence_folder = Path.home() / "projects" /"lipuma"/"sequences"/"180707"/"LiPumaStrains"
+
+	# import logging
+	_sequence_folder = Path.home() / "projects" / "lipuma" / "sequences" / "180707" / "LiPumaStrains"
 	project_folder = Path.home() / "projects" / "lipuma"
 	reference = project_folder / "AU1054" / "GCA_000014085.1_ASM1408v1_genomic.gbff"
-	reference_forward_read = project_folder / "sequences" / "071218_20"/ 'AU1581_S20_R1_001.fastq.gz'
-	reference_reverse_read = project_folder / "sequences" / "071218_20"/'AU1581_S20_R2_001.fastq.gz'
-	filename = project_folder /"samples.tsv"
-
+	sample_reference_id = "AU1836"
+	sample_table_filename = project_folder / "samples.tsv"
+	reference_forward_read, reference_reverse_read = get_sample(sample_reference_id, project_folder / "sequences")
 	parent_folder = project_folder / "cluster2_output"
 
 	if not parent_folder.exists():
 		parent_folder.mkdir()
 	print("Parent Folder: ", parent_folder)
-	#assemble_workflow(reference_forward_read, reference_reverse_read, parent_folder = parent_folder, genus = "Burkholderia", species = "cenocepacia")
+	# assemble_workflow(reference_forward_read, reference_reverse_read, parent_folder = parent_folder, genus = "Burkholderia", species = "cenocepacia")
 
 	# sampleName
 	# forwardRead
 	# reverseRead
 
-	table = pandas.read_csv(filename, sep = "\t")
+	table = pandas.read_csv(sample_table_filename, sep = "\t")
 	print("Found {} samples".format(len(table)))
-	print(table.to_string())
-	for index, row in table.iterrows():
-		#logging.info(f"{index} of {len(table)}")
+	#print(table.to_string())
+	assemble = True
+	if assemble:
+		assemble_workflow(reference_forward_read, reference_reverse_read,parent_folder, "burkholderia", "cenocepacia")
+	else:
+		for index, row in table.iterrows():
+			# logging.info(f"{index} of {len(table)}")
 
-		sample_name = row['sampleName']
-		forward = Path(row['forwardRead'])
-		reverse = Path(row['reverseRead'])
-		exists = forward.exists() and reverse.exists()
-		print(f"{index} of {len(table)}\t{sample_name}\t{exists}")
+			sample_name = row['sampleName']
+			forward = Path(row['forwardRead'])
+			reverse = Path(row['reverseRead'])
+			exists = forward.exists() and reverse.exists()
+			n = pendulum.now().to_datetime_string()
+			print(f"{n}:\t{index} of {len(table)}\t{sample_name}\t{exists}")
 
-		r = variant_call_workflow(sample_name, forward, reverse, parent_folder, reference)
+			r = variant_call_workflow(sample_name, forward, reverse, parent_folder, reference)
