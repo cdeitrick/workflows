d97b0769c53b22ed8759b6343640f6828626cc96
Unknown <cld100@pitt.edu>
2019-02-12 12:47:26 -0500

    fix

--- workflows/misc.py
+++ workflows/misc.py
@@ -2,9 +2,14 @@ from pathlib import Path
 import subprocess
 if __name__ == "__main__":
 	path = Path("/home/cld100/projects/lipuma/samples")
+	folders = list(path.iterdir())
+	for index, folder in enumerate(folders):
+		fastqs = [i for i in folder.iterdir() if 'R1' in i.name or 'R2' in i.name]
 
-	compressed_files = list(path.glob("**/*.gz"))
-	for index, compressed_file in enumerate(compressed_files):
-		print(f"{index} of {len(compressed_files)}: {compressed_file.name}")
-		command = ["gunzip", compressed_file]
-		subprocess.run(command)
+		cat_command = ["cat"] + fastqs
+		print(f"{index} of {len(folders)}: ", " ".join(map(str,cat_command)))
+		command = ["metaphlan2.py", "--input_type", "multifastq", "--bowtie2output", f"{folder.name}.bt2out.txt", "-o", f"{folder.name}.metaphlan.txt"]
+
+		process = subprocess.run(cat_command, stdout = subprocess.PIPE)
+
+		subprocess.run(command, stdin = process.stdout)
