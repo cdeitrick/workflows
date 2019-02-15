8da2caa5129f568398955b9f1e0c974f02173882
Unknown <cld100@pitt.edu>
2019-02-15 15:25:40 -0500

    fix

--- workflows/misc.py
+++ workflows/misc.py
@@ -2,7 +2,7 @@ import os
 import subprocess
 if __name__ == "__main__":
 	path = "/home/cld100/projects/lipuma/samples"
-	folders = list(os.listdir(path))
+	folders = list(i for i in os.listdir(path) if os.path.isdir(i))
 	for index, folder in enumerate(folders):
 		folder = path + '/' + folder
 		fastqs = [folder + '/' + i for i in os.listdir(folder) if 'R1' in os.path.basename(i) or 'R2' in os.path.basename(i)]
@@ -10,10 +10,14 @@ if __name__ == "__main__":
 		cat_command = ["cat"] + fastqs
 		print(index, len(folders), fastqs)
 		prefix = folder + '/' + os.path.basename(folder)
-		mcommand = ["metaphlan2.py", "--input_type", "multifastq", "--bowtie2out", prefix + ".bt2out.txt", "-o", prefix + ".metaphlan.txt"]
-		command = " ".join(cat_command) + " | " + " ".join(mcommand)
-		print(command)
-		os.system(command)
+		expected_output = prefix + ".metaphlan.txt"
+		if os.path.exists(expected_output):
+			print("Already exists: ", expected_output)
+		else:
+			mcommand = ["metaphlan2.py", "--input_type", "multifastq", "--bowtie2out", prefix + ".bt2out.txt", "-o", prefix + ".metaphlan.txt"]
+			command = " ".join(cat_command) + " | " + " ".join(mcommand)
+			print(command)
+			os.system(command)
 		#process = subprocess.run(cat_command, stdout = subprocess.PIPE, shell = True)
 
 		#subprocess.run(command, stdin = process.stdout)
