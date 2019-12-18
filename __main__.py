5b3bd25600360b0118d3536ad1f2eec034c861bd
cld100 <cld100@pitt.edu>
2019-12-18 11:56:45 -0500

    Patch to disable commandline

--- __main__.py
+++ __main__.py
@@ -1,14 +1,19 @@
 from pipelines import main
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-	"which",
-	help = "assembly or variants.",
-	type = str,
-	choices = ['assembly', 'variants']
-)
 
-args = parser.parse_args()
+def create_parser()->argparse.Namespace:
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		"which",
+		help = "assembly or variants.",
+		type = str,
+		choices = ['assembly', 'variants']
+	)
 
-main.main_shelly()
+	args = parser.parse_args()
+
+	return args
+if __name__ == "__main__":
+
+	main.main_shelly()
